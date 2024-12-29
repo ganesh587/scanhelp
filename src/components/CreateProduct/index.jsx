8f577fc06e50cd8deb1f590dfe70f59b864e5dc1
Jay Ganesh <jaya.ganesh@bridgera.com>
2024-12-29 19:58:40 +0530

    crate product

--- src/components/CreateProduct/index.jsx
+++ src/components/CreateProduct/index.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
 import config from '../../config';
 import { Helmet } from 'react-helmet';
 import Spinner from "../Spinner";
-import Message from '../Message'; 
+import Message from '../Message';
 
 const CreateProduct = () => {
   const [formData, setFormData] = useState({
@@ -16,22 +16,49 @@ const CreateProduct = () => {
     contact_phone: "",
     contact_alternate_number: "",
     contact_address: "",
-    note: "",
-    reward_amount: "",
-    tag_type: 1, // Default tag_type
-    emergency_contact: "",
-    blood_group: "",
-    existing_health_issues: "",
-    existing_medication: "",
-    primary_doctor: "",
-    allergies: "",
-    physically_disabled: false,
+    tag_type: 1
   });
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [userDataLoading, setUserDataLoading] = useState(true);
+  const navigate = useNavigate();
+
   const tag_type = localStorage.getItem("tag_type");
   const tag_id = localStorage.getItem("tag_id");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Get the token from localStorage
+    const token = localStorage.getItem("token");
+    const decodedToken = JSON.parse(atob(token.split('.')[1]));
+    const userId = decodedToken.user_id;
+
+    // Fetch user data
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`${config.API_URL}/users/${userId}/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        // Populate the form with the fetched user data
+        const { name, phone, alternate_number, address } = response.data;
+        setFormData((prevData) => ({
+          ...prevData,
+          contact_name: name || "",
+          contact_phone: phone || "",
+          contact_alternate_number: alternate_number || "",
+          contact_address: address || "",
+        }));
+        setUserDataLoading(false);
+      } catch (err) {
+        setUserDataLoading(false);
+        setError("Error fetching user data. Please try again.");
+      }
+    };
+
+    fetchUserData();
+  }, []); // Empty dependency array to run only once after mount
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -54,16 +81,7 @@ const CreateProduct = () => {
       contact_phone: formData.contact_phone,
       contact_alternate_number: formData.contact_alternate_number || null,
       contact_address: formData.contact_address || null,
-      note: tag_type === "1" ? formData.note : null,
-      reward_amount: tag_type === "1" ? formData.reward_amount : null,
-      owner: ownerId,
-      emergency_contact: tag_type === "2" ? formData.emergency_contact : null,
-      blood_group: tag_type === "2" ? formData.blood_group : null,
-      existing_health_issues: tag_type === "2" ? formData.existing_health_issues : null,
-      existing_medication: tag_type === "2" ? formData.existing_medication : null,
-      primary_doctor: tag_type === "2" ? formData.primary_doctor : null,
-      allergies: tag_type === "2" ? formData.allergies : null,
-      physically_disabled: tag_type === "2" ? formData.physically_disabled : false,
+      owner: ownerId
     };
 
     try {
@@ -86,102 +104,101 @@ const CreateProduct = () => {
   };
 
   return (
-    <> {error && <Message type="error" message={error} duration={5000} onClose={() => setError("")} />}
-    <div className={styles.create_product_container}>
-          <Helmet>
-        <title>Create Product</title>
-      </Helmet>
-      {error && <div className={styles.error_msg}>{error}</div>}
-      {loading && <Spinner />}
-      <form onSubmit={handleSubmit}>
-        <div className={styles.form_group}>
-          <label htmlFor="product_name">Product Name</label>
-          <input type="text" id="product_name" name="product_name" placeholder="Product Name" value={formData.product_name} onChange={handleChange} required />
-        </div>
+    <>
+      {error && <Message type="error" message={error} duration={5000} onClose={() => setError("")} />}
+      <div className={styles.create_product_container}>
+        <Helmet>
+          <title>Create Product</title>
+        </Helmet>
+        {error && <div className={styles.error_msg}>{error}</div>}
+        {loading && <Spinner />}
+        <form onSubmit={handleSubmit}>
+          <div className={styles.form_group}>
+            <label htmlFor="product_name">Product Name</label>
+            <input
+              type="text"
+              id="product_name"
+              name="product_name"
+              placeholder="Product Name"
+              value={formData.product_name}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="description">Product Description</label>
-          <textarea id="description" name="description" placeholder="Product Description" value={formData.description} onChange={handleChange} />
-        </div>
+          <div className={styles.form_group}>
+            <label htmlFor="description">Product Description</label>
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Product Description"
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </div>
 
-        {tag_type === "1" && (
-          <>
-            <div className={styles.form_group}>
-              <label htmlFor="reward_amount">Reward Amount</label>
-              <input type="number" id="reward_amount" name="reward_amount" placeholder="Reward Amount" value={formData.reward_amount} onChange={handleChange} />
-            </div>
-            <div className={styles.form_group}>
-              <label htmlFor="note">Note</label>
-              <textarea id="note" name="note" placeholder="Note" value={formData.note} onChange={handleChange} />
-            </div>
-          </>
-        )}
+          <div className={styles.form_group}>
+            <label htmlFor="contact_name">Contact Name</label>
+            <input
+              type="text"
+              id="contact_name"
+              name="contact_name"
+              placeholder="Contact Name"
+              value={formData.contact_name}
+              onChange={handleChange}
+            />
+          </div>
 
-        {tag_type === "2" && (
-          <>
-            <div className={styles.form_group}>
-              <label htmlFor="emergency_contact">Emergency Contact</label>
-              <input type="text" id="emergency_contact" name="emergency_contact" placeholder="Emergency Contact" value={formData.emergency_contact} onChange={handleChange} />
-            </div>
-            <div className={styles.form_group}>
-              <label htmlFor="blood_group">Blood Group</label>
-              <input type="text" id="blood_group" name="blood_group" placeholder="Blood Group" value={formData.blood_group} onChange={handleChange} />
-            </div>
-            <div className={styles.form_group}>
-              <label htmlFor="existing_health_issues">Existing Health Issues</label>
-              <input type="text" id="existing_health_issues" name="existing_health_issues" placeholder="Existing Health Issues" value={formData.existing_health_issues} onChange={handleChange} />
-            </div>
-            <div className={styles.form_group}>
-              <label htmlFor="existing_medication">Existing Medication</label>
-              <input type="text" id="existing_medication" name="existing_medication" placeholder="Existing Medication" value={formData.existing_medication} onChange={handleChange} />
-            </div>
-            <div className={styles.form_group}>
-              <label htmlFor="primary_doctor">Primary Doctor</label>
-              <input type="text" id="primary_doctor" name="primary_doctor" placeholder="Primary Doctor" value={formData.primary_doctor} onChange={handleChange} />
-            </div>
-            <div className={styles.form_group}>
-              <label htmlFor="allergies">Allergies</label>
-              <input type="text" id="allergies" name="allergies" placeholder="Allergies" value={formData.allergies} onChange={handleChange} />
-            </div>
-            <div className={styles.switch_container}>
-              <label>
-                Physically Disabled
-                <input type="checkbox" checked={formData.physically_disabled} onChange={() => setFormData(prevData => ({ ...prevData, physically_disabled: !prevData.physically_disabled }))} />
-              </label>
-            </div>
-          </>
-        )}
+          <div className={styles.form_group}>
+            <label htmlFor="contact_phone">Contact Phone</label>
+            <input
+              type="text"
+              id="contact_phone"
+              name="contact_phone"
+              placeholder="Contact Phone"
+              value={formData.contact_phone}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="contact_name">Contact Name</label>
-          <input type="text" id="contact_name" name="contact_name" placeholder="Contact Name" value={formData.contact_name} onChange={handleChange} />
-        </div>
+          <div className={styles.form_group}>
+            <label htmlFor="contact_alternate_number">Contact Alternate Number</label>
+            <input
+              type="text"
+              id="contact_alternate_number"
+              name="contact_alternate_number"
+              placeholder="Contact Alternate Number"
+              value={formData.contact_alternate_number}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="contact_phone">Contact Phone</label>
-          <input type="text" id="contact_phone" name="contact_phone" placeholder="Contact Phone" value={formData.contact_phone} onChange={handleChange} />
-        </div>
+          <div className={styles.form_group}>
+            <label htmlFor="contact_address">Contact Address</label>
+            <input
+              type="text"
+              id="contact_address"
+              name="contact_address"
+              placeholder="Contact Address"
+              value={formData.contact_address}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="contact_alternate_number">Contact Alternate Number</label>
-          <input type="text" id="contact_alternate_number" name="contact_alternate_number" placeholder="Contact Alternate Number" value={formData.contact_alternate_number} onChange={handleChange} />
-        </div>
+          <div className={styles.switch_container}>
+            <label>
+              Display
+              <input
+                type="checkbox"
+                checked={formData.display}
+                onChange={() => setFormData((prevData) => ({ ...prevData, display: !prevData.display }))}
+              />
+            </label>
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="contact_address">Contact Address</label>
-          <input type="text" id="contact_address" name="contact_address" placeholder="Contact Address" value={formData.contact_address} onChange={handleChange} />
-        </div>
-
-        <div className={styles.switch_container}>
-          <label>
-            Display
-            <input type="checkbox" checked={formData.display} onChange={() => setFormData(prevData => ({ ...prevData, display: !prevData.display }))} />
-          </label>
-        </div>
-        
-        <button type="submit">Create Product</button>
-      </form>
-    </div>
+          <button type="submit">Create Product</button>
+        </form>
+      </div>
     </>
   );
 };
