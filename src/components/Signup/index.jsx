f48da5b080792de163cbc8ebe17e7882cdf70707
Jay Ganesh <jaya.ganesh@bridgera.com>
2024-12-29 15:56:26 +0530

    register

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
-
+import ErrorMessage from '../ErrorMessage'; 
+import Spinner from "../Spinner";
+import { Helmet } from 'react-helmet';
+import config from '../../config';
 const Signup = () => {
   const [data, setData] = useState({
-    firstName: "",
-    lastName: "",
     email: "",
+    name: "",
     password: "",
+    phone: "",
+    address: "",
   });
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = ({ currentTarget: input }) => {
@@ -19,74 +24,92 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-      const url = "http://localhost:8080/api/users";
-      const { data: res } = await axios.post(url, data);
+      const url = `${config.API_URL}/signup/`;
+      const response = await axios.post(url, {
+        email: data.email,
+        name: data.name,
+        password: data.password,
+        phone: data.phone,
+        address: data.address,
+      });
+      setLoading(false);
       navigate("/app/login");
-      console.log(res.message);
     } catch (error) {
+      setLoading(false);
       if (
         error.response &&
         error.response.status >= 400 &&
         error.response.status <= 500
       ) {
-        setError(error.response.data.message);
+        setError(error.response.data.error);
       }
     }
   };
-
+  if (loading) return <Spinner />;
   return (
     <div className={styles.signup_container}>
+          <Helmet>
+        <title>Register</title> 
+      </Helmet>
+      {error && <ErrorMessage message={error} duration={5000} onClose={() => setError("")} />}
       <div className={styles.signup_form_container}>
         <div className={styles.left}>
           <h1>Welcome Back</h1>
-          <Link to='/app/login'>
-            <button type='button' className={styles.white_btn}>
+          <Link to="/app/login">
+            <button type="button" className={styles.white_btn}>
               Sign in
             </button>
           </Link>
         </div>
         <div className={styles.right}>
           <form className={styles.form_container} onSubmit={handleSubmit}>
-            <h1>Create Account</h1>
+            <h1>Register</h1>
             <input
-              type='text'
-              placeholder='First Name'
-              name='firstName'
-              onChange={handleChange}
-              value={data.firstName}
-              required
-              className={styles.input}
-            />
-            <input
-              type='text'
-              placeholder='Last Name'
-              name='lastName'
-              onChange={handleChange}
-              value={data.lastName}
-              required
-              className={styles.input}
-            />
-            <input
-              type='email'
-              placeholder='Email'
-              name='email'
+              type="email"
+              placeholder="Email"
+              name="email"
               onChange={handleChange}
               value={data.email}
               required
               className={styles.input}
             />
             <input
-              type='password'
-              placeholder='Password'
-              name='password'
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+              value={data.name}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
               onChange={handleChange}
               value={data.password}
               required
               className={styles.input}
             />
-            {error && <div className={styles.error_msg}>{error}</div>}
-            <button type='submit' className={styles.green_btn}>
+            <input
+              type="text"
+              placeholder="Phone"
+              name="phone"
+              onChange={handleChange}
+              value={data.phone}
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Address"
+              name="address"
+              onChange={handleChange}
+              value={data.address}
+              className={styles.input}
+            />
+            <button type="submit" className={styles.green_btn}>
               Sign Up
             </button>
           </form>
