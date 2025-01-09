7de490aef14c3e073d2c87042985d0b496dceb56
Jay Ganesh <jaya.ganesh@bridgera.com>
2025-01-09 17:13:02 +0530

    changed backend URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    API_URL: "https://related-danika-teja-791ca060.koyeb.app/api",
+    API_URL: "https://snh-backend-01-f36bf653c2cb.herokuapp.com/api",
   };
   
   export default config;
